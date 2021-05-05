48550fe5bac590eb629fc6b43fc45e095de79acc
Nisarga024 <nisargark2@gmail.com>
2021-05-05 17:55:52 +0530

    adding files

---
+++ JUnitTestProgram/src/SavingsAccount.java
@@ -0,0 +1,24 @@
+
+public class SavingsAccount {
+
+
+  private double bankBalance;
+    SavingsAccount(double bankBalance)
+    {
+    	super();this.bankBalance = bankBalance; 
+    	}
+    void withdraw(double amountToWithdraw)    {
+        System.out.println("Withdrawing..."+amountToWithdraw);
+        bankBalance = bankBalance - amountToWithdraw;
+        System.out.println("Balance : "+bankBalance);
+        //bankBalance = bankBalance - 20; // some coding fraud
+    }
+    public double getBankBalance() {   
+    	return bankBalance;  
+    	}
+    @Override public String toString() {
+        return "SavingsAccount [bankBalance=" + bankBalance + "]";
+    }
+    
+    
+}
