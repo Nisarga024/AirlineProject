b7a36a4ba1f75241bda4df35e3871cd5420569ea
Nisarga024 <83340697+Nisarga024@users.noreply.github.com>
2021-05-11 17:38:27 +0530

    Create CustomerService.java

---
+++ CustomerService.java
@@ -0,0 +1,9 @@
+package com.service;
+
+public interface CustomerService {
+	
+	public void applyForChequeBook(long acno);
+    public void stopCheque(long acno);
+    public void applyForCreditCard(String name, double salary);
+    public double balance(long acno);
+}
